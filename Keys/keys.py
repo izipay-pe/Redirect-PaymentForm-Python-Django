49683125958f1bef6bc1f76114a46643e94b12d8
izipay-pe <103148193+izipay-pe@users.noreply.github.com>
2024-02-21 12:52:53 -0500

    Update keys.py

--- Keys/keys.py
+++ Keys/keys.py
@@ -1,6 +1,11 @@
 keys = {
-    "username": "83041791",
-    "password": "testpassword_Q5Bkt5jkR3bmbsiqiiX4tMBmCrZNU6ykrPnaR6rVnlP6G",
-    "publickey": "83041791:testpublickey_IOcZtEB9CaYODZINrQA0Kdm0gow6qdsmTiwe4oIpAnPft",
-    "HMACSHA256": "coa7sEWh0W9OXMYeOhDl2OdQcy2CCLjlNSePkSRMkaD6q"
+    "username" : "~~CHANGE_ME_USERNAME~~",
+    "password": "~~CHANGE_ME_PASSWORD~~",
+    "publickey" : "~~CHANGE_ME_PUBLIC_KEY~~",
+    "HMACSHA256" : "~~CHANGE_ME_HMAC~~",
 }
+
+
+
+
+   
